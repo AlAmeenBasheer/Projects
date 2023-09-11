e37b921bd2e2253b137ad2434eda296d756dd9f0
Al Ameen Basheer M B <134540107+AlAmeenBasheer@users.noreply.github.com>
2023-09-11 17:47:42 +0530

    Add files via upload

---
+++ WrapperEx.java
@@ -0,0 +1,13 @@
+package com.test.Testcore1;
+
+public class WrapperEx {
+
+	public static void main(String[] args) {
+		int a=10;// primitive data type
+		Integer b=40; // Wrapper class
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
